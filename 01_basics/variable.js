1ca722f27e1c45313339565ab91dd9744a9472b2
Basirul Khan <141140812+basirulkhan6646@users.noreply.github.com>
2024-02-27 18:16:17 +0000

    worked on JS variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accounEmail ="basirulkhan007@gmail.com"
+var accountName="Basirul"
+accountCity="Kolkata"
+
+let accountState
+
+// accountId =2344
+accounEmail ="basirul.k@yopmail.com"
+accountName ="B khan"
+accountCity="Kopa"
+
+console.log(accountId);
+console.table([accountId,accounEmail,accountName,accountCity,accountState])
