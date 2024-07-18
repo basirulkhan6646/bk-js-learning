8b5e41595e803830ae1fa1f1366aca197ab25da9
Basirul Khan <141140812+basirulkhan6646@users.noreply.github.com>
2024-07-18 17:30:52 +0000

    basics of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as  newer version
+
+//alert ("Hello BK")  //we can't use on node js, but can use in browser
+
+//DataTypes
+
+let name ="Basirul"
+let age = 26
+let isLoggedIn = false;
+let country;
+
+
+//1. String => inside double quotation  " " 
+//2. Number => 2 to power 53
+//3. bigint 
+//4. bolean => true/false
+//5. null => standalone value (Empty) , it's typeof is object
+//6. undefined =>  no value assigned 
+//7. symbol => unique
+//8. object  =>
+
+
+    console.log(typeof null);
