685215a4e450dbf3fa06ca3b8e4a140a35d34d76
Basirul Khan <141140812+basirulkhan6646@users.noreply.github.com>
2024-07-18 18:07:38 +0000

    data converstions

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,40 @@
+let score
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+/*
+"score" value input
+ "33" => 33 /type: number
+ "33asd" => NaN / type: number
+  true => 1, false=> 0 / type : number
+  null=> 0 / type:number
+  undefined => NaN / type:numver
+*/
+
+
+let isLoggedIn = undefined ;
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(boolIsLoggedIn);
+
+/* 
+isLoggedIn input value
+ 1 =>true
+ 0 =>false
+ any number value => true
+ null/undefined => false
+ "" => false
+ "Hello" => true
+*/
+
+
+let myAge= true ;
+
+let stringMyAge = String (myAge);
+
+console.log(typeof (stringMyAge))
+console.log(stringMyAge);
